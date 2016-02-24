3cdab4cbef5c80bbfd74785a10e794ab2b239fb3
Eoin <eoinkane1@gmail.com>
2016-02-24 15:53:21 +0000

    homepage half way finished and contact page started

---
+++ infusedireland/js/instafeed.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: '45fe52f3d5254c1c96bb489ee223e8a0'
+    });
+    feed.run();
+</script>
